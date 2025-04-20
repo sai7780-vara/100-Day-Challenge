0b11e2b88992e7feaf732b0a222483d54f73e509
sai7780-vara <rukkamolla.sai@gmail.com>
2025-04-19 21:16:23 -0500

    Time: 2203 ms (16.17%), Space: 13.4 MB (16.3%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        a=[]
+        l=len(nums)
+        for i in range(l):
+            for j in range(i+1,l):
+                if nums[i]+nums[j]==target:
+                    a.append(i)
+                    a.append(j)
+
+
+        return a
+                
+        
