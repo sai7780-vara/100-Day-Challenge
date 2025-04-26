a71fc1c0e5448711900882a54cf5645997265360
SAI KIRAN REDDY <rukkamolla.sai@gmail.com>
2025-04-25 21:54:39 -0500

    Time: 7 ms (84.04%), Space: 13.1 MB (40.69%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,19 +1,12 @@
 class Solution(object):
     def canPlaceFlowers(self, f, n):
-        k=len(f)
-        s=0
-        if k==1 and f[0]==0:
-            s=s+1
-        
-        for i in range(k-1):
-            if i==0:
-                if f[0]==0 and f[1]==0:
-                    s=s+1
-                    f[0]=1
-            if f[i]==0 and f[i-1]==0 and f[i+1]==0:
-                s=s+1
-                f[i]=1
-        if f[k-1]==0 and f[k-2]==0:
-            s=s+1
-            
-        return s>=n
+        k = len(f)
+        s = 0
+        for i in range(k):
+            if f[i] == 0:
+                empty_left = (i == 0) or (f[i-1] == 0)
+                empty_right = (i == k-1) or (f[i+1] == 0)
+                if empty_left and empty_right:
+                    f[i] = 1
+                    s += 1
+        return s >= n
